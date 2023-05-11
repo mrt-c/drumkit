b25d19bfd6d9cbd4ff7caca4625caae71bce45a5
mrt-c <70955831+mrt-c@users.noreply.github.com>
2023-05-11 18:30:33 +0400

    Add files via upload

---
+++ drumkit/script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector("body")
+body.addEventListener("click",function (event) {
+    let target = event.target
+    
+    if (target.dataset.audioId!==undefined){
+    }
+})
